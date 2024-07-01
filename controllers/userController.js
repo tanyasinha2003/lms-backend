d7a80df49c2e57b22251d478d8dd375a82564df7
tanyasinha2003 <tanyasinha2003@gmail.com>
2024-07-01 23:44:38 +0530

    updated query

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,8 +18,8 @@ const login = async (req, res) => {
   try {
     // Find user in database
     console.log("before user find one query");
-    const user = await User.findOne({ email }).explain();
-console.log(user);
+    const user = await User.findOne({ email }).limit(1);
+
     // const user = await User.findOne({ email }).limit(1);
 
     console.log("after user find one query");

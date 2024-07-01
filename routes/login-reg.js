09863f8947385d230852d9f18c6275057947e151
tanyasinha2003 <tanyasinha2003@gmail.com>
2024-07-01 22:47:09 +0530

    naming updated

--- routes/login-reg.js
+++ routes/login-reg.js
@@ -1,5 +1,5 @@
 
-import authController from '../controllers/userController.js'
+import userController from '../controllers/userController.js'
 import express from 'express';
 import verifyToken from '../middlewares/authMiddleware.js';
 const router = express.Router();
@@ -8,16 +8,12 @@ const router = express.Router();
 
 
 
-// router.post('/login', authController.login);
-
-router.post('/login', (req, res) => {
-    res.send('Login endpoint reached');
-});
+router.post('/login', userController.login);
 
 
 // Route to register a new user
-router.post('/register', authController.register);
+router.post('/register', userController.register);
 
-router.get('/userDetails',verifyToken,authController.getUser);
+router.get('/userDetails',verifyToken,userController.getUser);
 
 export default router;
